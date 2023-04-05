151c21d8ddf8eb6b733ebed1ae982dafeeefcc58
MuriloSG <muriloprojetosti@gmail.com>
2023-04-05 10:38:29 -0300

    estilizando o card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
+import './App.css';
 import Header from './components/Header/index.js';
 import Hash from "./components/Hash/index.js";
 
-function App() {
-  return (
-    <>
+const App = () => (
+    <main className="app">
       <Header />
       <Hash />
-    </>
+    </main>
   );
-}
 
 export default App;
